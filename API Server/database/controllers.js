260a59b9f951d5a90e4cb1e152c4ec14657a73bd
Nika Woodfill <nikawoodfill@nikas-air-2.myfiosgateway.com>
2023-01-07 15:15:00 -0500

    Artillery testing added

--- API Server/database/controllers.js
+++ API Server/database/controllers.js
@@ -1,117 +1,95 @@
-const Products = require('./db.js');
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient({
+  log: [
+    {
+      emit: "event",
+      level: "query",
+    },
+    {
+      emit: "stdout",
+      level: "error",
+    },
+    {
+      emit: "stdout",
+      level: "info",
+    },
+    {
+      emit: "stdout",
+      level: "warn",
+    },
+  ],
+});
+
+prisma.$on("query", (e) => {
+  console.log("Query: " + e.query);
+  console.log("Params: " + e.params);
+  console.log("Duration: " + e.duration + "ms");
+});
 
 async function main() {
-    exports.getProducts = (query) => { 
-        const page = query['page'] || 1;
-        const count = parseInt(query['count']) || 5;
-        const amountToSkip = page == 1? 0: count * (page-1);
-        return prisma.products.findMany({
-                take : count,
-                skip : amountToSkip
-        })
-    }
+  exports.getProducts = (query) => {
+    const page = query["page"] || 1;
+    const count = parseInt(query["count"]) || 5;
+    const amountToSkip = page == 1 ? 0 : count * (page - 1);
+    return prisma.products.findMany({
+      take: count,
+      skip: amountToSkip,
+    });
+  };
 
-
-    exports.getProductInformation = (id) => {
-        const product_id = parseInt(id)
-        return prisma.products.findUnique({
-            where: {
-              id: product_id,
-            },
-            include: {
-              features: {
-                select: {
-                  feature: true,
-                  value: true  
-                }
-              }
-            }
-        })
-
-    }   
-
-
-    exports.getStyles = (id) => {
-        const product_id = parseInt(id);
-        return prisma.styles.findMany({
-        where: {
-            product_id: product_id,
+  exports.getProductInformation = (id) => {
+    const product_id = parseInt(id);
+    return prisma.products.findUnique({
+      where: {
+        id: product_id,
+      },
+      include: {
+        features: {
+          select: {
+            feature: true,
+            value: true,
+          },
         },
-        
-        select: {
-            style_id: true,
-            name: true,
-            original_price: true,
-            sale_price : true,
-            default_style : true,
-            photos: {
-                select: {
-                    thumbnail_url: true,
-                    url: true 
-                },
-            },
-            skus : {
-                select: {
-                    id : true,
-                    quantity: true,
-                    size: true
-                },
-            }
-         
+      },
+    });
+  };
+
+  exports.getStyles = (id) => {
+    const product_id = parseInt(id);
+    return prisma.styles.findMany({
+      where: {
+        product_id: product_id,
+      },
+      select: {
+        style_id: true,
+        name: true,
+        original_price: true,
+        sale_price: true,
+        default_style: true,
+        photos: {
+          select: {
+            thumbnail_url: true,
+            url: true,
+          },
         },
-      })
-
-    }
-
-    exports.formatStyles = (styles) => {
-        let obj = [];
-        for (var i = 0 ; i< styles.length; i++) {
-            let skus = {};
-            for (var j = 0 ; j < styles[i]['skus'].length; j++) {
-                skus[styles[i]['skus'][j]['id']] = 
-                    {
-                    'quantity': styles[i]['skus'][j]['quantity'],
-                    'size': styles[i]['skus'][j]['size']
-
-                    }
-            }
-            obj.push(styles[i])
-            obj[i]['skus'] = skus
-        }
-        return obj;
-    }
-
-    exports.getRelatedProducts = (id) => {
-        const product_id = parseInt(id);
-        return prisma.relatedproducts.findMany ({
-        where: {
-            related_product_id: product_id,
+        skus: {
+          select: {
+            id: true,
+            quantity: true,
+            size: true,
+          },
         },
-        select : {
-            current_product_id: true
-        }
-    })
-    }
-
-    exports.formatRelatedProducts = (relatedProducts) => {
-        let array = [];
-        for (var i of relatedProducts) {
-            array.push(i['current_product_id'])
-        }
-        return array;
-    }
-
-
-
+      },
+    });
+  };
 }
-  main()
-    .then(async () => {
-      await prisma.$disconnect()
-    })
-    .catch(async (e) => {
-      console.error(e)
-      await prisma.$disconnect()
-      process.exit(1)
-    })
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
--- API Server/server/index.js
+++ API Server/server/index.js
@@ -1,36 +1,31 @@
-var express = require('express')
-var app = express()
-const {port} = require('../config');
-const controllers = require('../database/controllers.js');
-var url = require('url');
+var express = require("express");
+var app = express();
+const { port } = require("../config");
+const controllers = require("../database/controllers.js");
 
-// Start server and listen on port 
+// Start server and listen on port
 app.listen(port, function () {
-    console.log(`listening on port ${port}`);
-}); 
+  console.log(`listening on port ${port}`);
+});
 
+app.get("/products", async (req, res) => {
+  const products = await controllers.getProducts(req.query);
+  res.status(200).json(products);
+});
 
-app.get('/products', async (req, res) => {
-    const products = await controllers.getProducts(req.query)
-    res.status(200).json(products)
-})
+app.get("/products/:id", async (req, res) => {
+  const productInformation = await controllers.getProductInformation(
+    req.params.id
+  );
+  res.status(200).json(productInformation);
+});
 
-app.get('/products/:id', async (req, res) => {
-    const productInformation = await controllers.getProductInformation(req.params.id)
-    res.status(200).json(productInformation)
-})
+app.get("/products/:id/styles", async (req, res) => {
+  const styles = await controllers.getStyles(req.params.id);
+  res.status(200).json(styles);
+});
 
-
-app.get('/products/:id/styles', async (req, res) => {
-    const styles = await controllers.getStyles(req.params.id)
-    const formattedStyles = controllers.formatStyles(styles)
-    res.status(200).json(formattedStyles)
-
-})
-
-app.get('/products/:id/related', async (req, res) => {
-    const relatedProducts = await controllers.getRelatedProducts(req.params.id)
-    const formattedRelatedProducts = controllers.formatRelatedProducts(relatedProducts)
-    res.status(200).json(formattedRelatedProducts)
-
-})
+app.get("/products/:id/related", async (req, res) => {
+  const relatedProducts = await controllers.getRelatedProducts(req.params.id);
+  res.status(200).json(relatedProducts);
+});
